f833edf584796cbebf81370be9f3f040611c572e
Philip Verbist <philip.verbist@gmail.com>
2016-10-11 01:21:37 +0200

    small fixes, working UserController

--- config/web.php
+++ config/web.php
@@ -3,9 +3,10 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'rest',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'user/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -38,14 +39,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
--- controllers/ApiController.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace app\controllers;
-
-class ApiController extends \verbi\yii2ExtendedRestController {
-    
-}
